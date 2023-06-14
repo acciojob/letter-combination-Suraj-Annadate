a51651dbd2a1c3ad79992340543cf98e11b53bae
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-14 12:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const mapping = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+	function combination(index, curr, combi)
+		{
+			if(index===input_digit.length)
+			{
+			combi.push(curr)
+				return
+			}
+			const digit = input_digit[index]
+			const letters = mapping[input_digit]
+
+			for(let i=0;i<letters.length;i++)
+				{
+					const letter = letters[i]
+					combination(index+1,curr+letter,combi)
+				}
+		}
+	const combinations=[]
+	if(input_digit.length>0)
+	{
+		combination(0,"",combinations)
+	}
+	return combinations.sort()
 }
 
 module.exports = letterCombinations;
